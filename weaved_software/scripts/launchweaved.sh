3d68e33f83663599c1f1d27453bd84c93946dc9c
Mike Young <myoung@wildernessvoice.com>
2014-12-21 10:04:05 -0800

    new start/stop format

--- weaved_software/scripts/launchweaved.sh
+++ weaved_software/scripts/launchweaved.sh
@@ -1,5 +1,14 @@
-#! /bin/bash
+#!/bin/bash
 
+#  launchweaved.sh
+#  
+#
+#  Weaved, Inc. Copyright 2014. All rights reserved.
+#
+
+VERSION="v1.0"
+AUTHOR="Mike Young"
+MODIFIED="December 27, 2014"
 WEAVED_PORT=
 DAEMON=weavedConnectd
 WEAVED_DIR=/etc/weaved/services
@@ -11,28 +20,60 @@ LOG_FILE=/dev/null
 LOG_DIR=~/
 
 
-if [ -z "$1" ]; then
-	echo "You need one of the following arguments: start|stop|restart"
-	exit
-fi 
-if [ "$(echo "$1" | tr '[A-Z]' '[a-z]' | tr -d ' ')" = "stop" ]; then 
-	echo "Stopping $WEAVED_PORT"
-	sudo kill "$(ps ax | grep $WEAVED_PORT | grep -v grep | awk '{print $1}')"
-	sudo rm $PIDPATH
-elif [ "$(echo "$1" | tr '[A-Z]' '[a-z]' | tr -d ' ')" = "start" ]; then
-	if [ -z "$(ps ax | grep $WEAVED_PORT | grep -v grep)" ]; then
-		echo "Starting $WEAVED_PORT"
+checkPID()
+{
+	if [ -f $PIDPATH ]; then
+		runningPID="$(cat $PIDPATH)"
+	fi
+}
+
+isRunning()
+{
+	isRunning="$(ps ax | grep weaved | grep $WEAVED_PORT | grep -v grep | wc -l)"
+}
+
+stopWeaved()
+{
+	isRunning
+	checkPID
+	if [ $isRunning != 0 ]; then
+		echo "Stopping $WEAVED_PORT..."
+		sudo kill $runningPID 2> /dev/null
+		sudo rm $PIDPATH 2> /dev/null
+	else
+		echo "$WEAVED_PORT is not currently active. Nothing to stop."
+	fi
+}
+
+startWeaved()
+{
+	isRunning
+	if [ $isRunning = 0 ]; then
+		echo "Starting $WEAVED_PORT..."
 		sudo $BIN_DIR/$DAEMON -f $WEAVED_DIR/$WEAVED_PORT.conf -d $PID_DIR/$WEAVED_PORT.pid > $LOG_DIR/$WEAVED_PORT.log
+		tail $LOG_DIR/$WEAVED_PORT.log
 	else
 		echo "$WEAVED_PORT is already started"
 	fi
-elif [ "$(echo "$1" | tr '[A-Z]' '[a-z]' | tr -d ' ')" = "restart" ]; then
-	echo "Stopping $WEAVED_PORT"
-	sudo kill "$(ps ax | grep $WEAVED_PORT | grep -v grep | awk '{print $1}')"
-	sudo rm $PIDPATH
-	echo "Starting $WEAVED_PORT"
-	sudo $BIN_DIR/$DAEMON -f $WEAVED_DIR/$WEAVED_PORT.conf -d $PID_DIR/$WEAVED_PORT.pid > $LOG_DIR/$WEAVED_PORT.log
+}
 
+restartWeaved()
+{
+	stopWeaved
+	sleep 2
+	startWeaved
+}
+
+if [ -z $1 ]; then
+	echo "You need one of the following arguments: start|stop|restart"
+	exit
+elif [ "$(echo "$1" | tr '[A-Z]' '[a-z]' | tr -d ' ')" = "stop" ]; then 
+	stopWeaved
+elif [ "$(echo "$1" | tr '[A-Z]' '[a-z]' | tr -d ' ')" = "start" ]; then
+	startWeaved
+elif [ "$(echo "$1" | tr '[A-Z]' '[a-z]' | tr -d ' ')" = "restart" ]; then
+	restartWeaved
 else
 	echo "This option is not supported"
 fi
+
